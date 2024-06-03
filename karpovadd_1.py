63ec1f0bc2343dd93d1309c677cd798e3f51ac7d
KD123455 <116819724+KD123455@users.noreply.github.com>
2024-06-03 20:32:33 +0300

    karpovadd_1.py

--- karpovadd_1.py
+++ karpovadd_1.py
@@ -1,16 +1,17 @@
 s1=[1,2,3,4,5,6,8,9]
 s2=[4,5,6,7]
-s3=[]
+res=[]
 a=len(s1)
 b=len(s2)
 for x in s1:
-    s3.append(x)
+    res.append(x)
 for x in s2:
-    s3.append(x)
-print(s3)
-  
+    res.append(x)
+print('начальные списки')
+print(s1)
+print(s2)  
 for i in range(a+b-1):
     for j in range(a+b-1-i):
-        if s3[j]>s3[j+1]:
-            s3[j], s3[j+1] = s3[j+1], s3[j]
-print(s3)
+        if res[j]>res[j+1]:
+            res[j], res[j+1] = res[j+1], res[j]
+print(res)
